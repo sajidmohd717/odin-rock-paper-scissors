2b95c5df31f85a14fb1d37145d0bf43b4f521916
Sajid <sajidmohammad460519@gmail.com>
2024-04-20 16:08:50 +0800

    improved UI trastically, if want to work on this again also can, but for now, it's good, can play the game on the website if you want to

--- index.js
+++ index.js
@@ -1,43 +1,33 @@
+// Variables
+
 let playerpts = 0
 let computerpts = 0
-
-
-
-function getComputerselection() {
-  let availableselections = ["rock", "paper", "scissors"];
-  let compSelection = Math.floor(Math.random() * availableselections.length);
-
-  console.log(availableselections[compSelection]);
-  return availableselections[compSelection];
-}
-
-function updateCurrentPoints(playerpts, computerpts) {
-  const playerPoints = document.querySelector('.playerPoints')
-  const ComputerPoints = document.querySelector('.computerPoints')
-  const pointsTable = document.querySelector('.pointsTable')
-
-  playerPoints.textContent = "player points: " + playerpts;
-  ComputerPoints.textContent = "comptuer points: " + computerpts;
-
-}
-
 
 let buttons = document.querySelectorAll('button')
 let gameBodyDiv = document.querySelector('.gameBodyDiv')
 let gameResult;
 
+let round_no = 0
+
+
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     playerSelection = button.id
 
-    let computerSelection = getComputerselection()
+    clearBody()
+
+    let computerSelection = getComputerSelection()
     let gameResult = document.createElement('h3')
 
     const playerChoiceP = document.createElement('p')
     const computerChoiceP = document.createElement('p')
-    playerChoiceP.textContent = "you choose: " + playerSelection
+    const roundNumber = document.createElement('p')
+    roundNumber.textContent = "Round " + round_no
+    round_no ++;
+    gameBodyDiv.appendChild(roundNumber)
+    playerChoiceP.textContent = "you chose: " + playerSelection
     gameBodyDiv.appendChild(playerChoiceP)
-    computerChoiceP.textContent = "computer choose: " + computerSelection
+    computerChoiceP.textContent = "computer chose: " + computerSelection
     gameBodyDiv.appendChild(computerChoiceP)
     
 
@@ -61,3 +51,25 @@ buttons.forEach((button) => {
   })
 })
 
+function getComputerSelection() {
+  let availableselections = ["rock", "paper", "scissors"];
+  let compSelection = Math.floor(Math.random() * availableselections.length);
+
+  console.log(availableselections[compSelection]);
+  return availableselections[compSelection];
+}
+
+function updateCurrentPoints(playerpts, computerpts) {
+  const playerPoints = document.querySelector('.playerPoints')
+  const ComputerPoints = document.querySelector('.computerPoints')
+  const pointsTable = document.querySelector('.pointsTable')
+
+  playerPoints.textContent = "player points: " + playerpts;
+  ComputerPoints.textContent = "comptuer points: " + computerpts;
+
+}
+
+function clearBody()
+{
+    document.querySelector(".gameBodyDiv").innerHTML = "";
+}
